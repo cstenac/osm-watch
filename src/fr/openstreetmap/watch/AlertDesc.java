66e15aaf3d8d0f2eab391965e29f5afa23fc54a5
Clément Stenac <clement.stenac@exalead.com>
2012-09-10 12:07:29 +0200

    Add the whole JPA / Hibernate / SQLite stuff.
    SOooo boring

---
+++ src/fr/openstreetmap/watch/AlertDesc.java
@@ -0,0 +1,47 @@
+package fr.openstreetmap.watch;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AlertDesc {
+  public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String getUniqueKey() {
+        return uniqueKey;
+    }
+    public void setUniqueKey(String uniqueKey) {
+        this.uniqueKey = uniqueKey;
+    }
+    public String getWatchedTags() {
+        return watchedTags;
+    }
+    public void setWatchedTags(String watchedTags) {
+        this.watchedTags = watchedTags;
+    }
+    public String getPolygonWKT() {
+        return polygonWKT;
+    }
+    public void setPolygonWKT(String polygonWKT) {
+        this.polygonWKT = polygonWKT;
+    }
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String user;
+    private String uniqueKey;
+    private String watchedTags;
+    private String polygonWKT;
+}
