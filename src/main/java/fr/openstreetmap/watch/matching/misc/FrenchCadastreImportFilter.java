9fb1e7eb3ca49337d0b1c012b540eb2fdf817d1c
Clément Stenac <clement.stenac@gmail.com>
2012-10-13 14:33:38 +0200

    * French Cadastre import detection filter * CLeanup

---
+++ src/main/java/fr/openstreetmap/watch/matching/misc/FrenchCadastreImportFilter.java
@@ -0,0 +1,42 @@
+package fr.openstreetmap.watch.matching.misc;
+
+import org.apache.log4j.Logger;
+
+import fr.openstreetmap.watch.matching.Filter;
+import fr.openstreetmap.watch.matching.MatchDescriptor;
+import fr.openstreetmap.watch.matching.SpatialMatch;
+import fr.openstreetmap.watch.model.WayDescriptor;
+
+
+/**
+ * Detects imports that look like french cadastre building imports. 
+ */
+public class FrenchCadastreImportFilter extends Filter {
+	public FrenchCadastreImportFilter(String params) {}
+	
+	@Override
+	public MatchDescriptor matches(SpatialMatch changeset) {
+		MatchDescriptor md = new MatchDescriptor(changeset);
+		int nbCadastreBuildings = 0;
+		int nbBuildings = 0;
+		
+		for (WayDescriptor wd : changeset.newWays) {
+			if (wd.hasTag("building")) {
+				nbBuildings++;
+				String source = wd.getSourceTag();
+				if (source != null && source.contains("cadastre-dgi")) {
+					nbCadastreBuildings++;
+				}
+			}
+		}
+		
+		logger.info("newWays=" + changeset.newWays.size() +  " buildings=" + nbBuildings +  " cadastre=" + nbCadastreBuildings);
+		
+		if (nbCadastreBuildings > 40) {
+			md.matches = true;
+			md.reasons.add("More than 40 buildings with Cadastre source");
+		}
+		return md;
+	}
+	private static Logger logger = Logger.getLogger("osm");
+}
---
+++ src/main/java/fr/openstreetmap/watch/matching/misc/LegacyTagKeysFilter.java
@@ -0,0 +1,108 @@
+package fr.openstreetmap.watch.matching.misc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import fr.openstreetmap.watch.matching.Filter;
+import fr.openstreetmap.watch.matching.MatchDescriptor;
+import fr.openstreetmap.watch.matching.SpatialMatch;
+import fr.openstreetmap.watch.model.NodeChange;
+import fr.openstreetmap.watch.model.NodeDescriptor;
+import fr.openstreetmap.watch.model.WayChange;
+import fr.openstreetmap.watch.model.WayDescriptor;
+
+/**
+ * Legacy/example filter based on tag keys.
+ */
+public class LegacyTagKeysFilter extends Filter {
+	Set<String> watchedKeys;
+	public LegacyTagKeysFilter(String watchedKeys) {
+		Set<String> set = new HashSet<String>();
+		for (String s : watchedKeys.split(",")) set.add(s);
+		this.watchedKeys = set;
+	}
+	@Override
+	public MatchDescriptor matches(SpatialMatch sm) {
+		MatchDescriptor md = new MatchDescriptor(sm);
+		for (NodeDescriptor n : sm.newNodes) {
+			if (n.tags != null) {
+				for (String k : n.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addNode(n, "New node has watched tag " + k);
+					}
+				}
+			}
+		}
+		for (NodeDescriptor n : sm.deletedNodes) {
+			if (n.tags != null) {
+				for (String k : n.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addNode(n, "Deleted node has watched tag " + k);
+					}
+				}
+			}
+		}
+		for (NodeChange n : sm.changedNodes) {
+			if (n.after.tags != null) {
+				for (String k : n.after.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addNode(n.after, "Changed node has watched tag AFTER " + k);
+					}
+				}
+			}
+			if (n.before.tags != null) {
+				for (String k : n.after.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addNode(n.before, "Changed node has watched tag BEFORE " + k);
+					}
+				}
+			}
+		}
+
+
+		for (WayDescriptor n : sm.newWays) {
+			if (n.tags != null) {
+				for (String k : n.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addWay(n, "New way has watched tag " + k);
+					}
+				}
+			}
+		}
+		for (WayDescriptor n : sm.deletedWays) {
+			if (n.tags != null) {
+				for (String k : n.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addWay(n, "Deleted way has watched tag " + k);
+					}
+				}
+			}
+		}
+		for (WayDescriptor n : sm.waysWithChangedNodes) {
+			if (n.tags != null) {
+				for (String k : n.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addWay(n, "Way with changed nodes has watched tag " + k);
+					}
+				}
+			}
+		}
+		for (WayChange n : sm.changedWays) {
+			if (n.after.tags != null) {
+				for (String k : n.after.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addWay(n.after, "Changed way has watched tag AFTER " + k);
+					}
+				}
+			}
+			if (n.after.tags != null) {
+				for (String k : n.after.tags.keySet()) {
+					if (watchedKeys.contains(k)) {
+						md.addWay(n.before, "Changed way has watched tag BEFORE " + k);
+					}
+				}
+			}
+		}
+		return md;
+	}
+}
