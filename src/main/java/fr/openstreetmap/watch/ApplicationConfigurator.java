4fdf68f4be643377ed76c878e736b90a4aa77953
Clément Stenac <clement.stenac@gmail.com>
2012-10-28 15:52:33 +0100

    Send the mails !

--- src/main/java/fr/openstreetmap/watch/ApplicationConfigurator.java
+++ src/main/java/fr/openstreetmap/watch/ApplicationConfigurator.java
@@ -14,41 +14,48 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationConfigurator implements ApplicationContextAware {
-    @Override
-    public void setApplicationContext(ApplicationContext arg0) throws BeansException {
-        System.out.println("******* Starting up ********");
-        //        Logger.getRootLogger().removeAllAppenders();
-        //        BasicConfigurator.configure();
-    }
+	@Override
+	public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+		System.out.println("******* Starting up ********");
+		//        Logger.getRootLogger().removeAllAppenders();
+		//        BasicConfigurator.configure();
+	}
 
-    static Properties config;
+	static Properties config;
 
-    private static void parseConfig() {
-        try {
-            if (config == null) {
-                config = new Properties();
-                config.load(new FileInputStream(new File(System.getProperty("user.home") + "/osm-watch.properties")));
-            }
-        } catch (IOException e) {
-            throw new Error(e);
-        }
-    }
+	private static void parseConfig() {
+		if (config == null) {
+			try {
+				config = new Properties();
+				config.load(new FileInputStream(new File(System.getProperty("user.home") + "/osm-watch.properties")));
+			} catch (IOException e) {
+				throw new Error(e);
+			}
+		}
 
-    public static Map<String, String> getPersistenceProperties() {
-        parseConfig();
-        Map<String, String> p = new HashMap<String, String>();
-        if (!config.containsKey("dialect")) throw new Error("Missing 'dialect' key in config");
-        if (!config.containsKey("driver")) throw new Error("Missing 'driver' key in config");
-        if (!config.containsKey("jdbcurl")) throw new Error("Missing 'jdbcurl' key in config");
-        p.put("hibernate.dialect", config.getProperty("dialect"));
-        p.put("hibernate.connection.driver_class", config.getProperty("driver"));
-        p.put("hibernate.connection.url", config.getProperty("jdbcurl"));
-        return p;
-    }
+	}
 
-    public static synchronized String getBaseURL() {
-        parseConfig();
-        return config.getProperty("baseurl");
-    }
+	public static synchronized Map<String, String> getPersistenceProperties() {
+		parseConfig();
+		Map<String, String> p = new HashMap<String, String>();
+		if (!config.containsKey("dialect")) throw new Error("Missing 'dialect' key in config");
+		if (!config.containsKey("driver")) throw new Error("Missing 'driver' key in config");
+		if (!config.containsKey("jdbcurl")) throw new Error("Missing 'jdbcurl' key in config");
+		p.put("hibernate.dialect", config.getProperty("dialect"));
+		p.put("hibernate.connection.driver_class", config.getProperty("driver"));
+		p.put("hibernate.connection.url", config.getProperty("jdbcurl"));
+		return p;
+	}
+
+	public static synchronized String getMandatoryProperty(String key) {
+		parseConfig();
+		if (!config.containsKey("key")) throw new Error("Missing '" + key + "' key in config");
+		return config.getProperty(key);
+	}
+
+	public static synchronized String getBaseURL() {
+		parseConfig();
+		return config.getProperty("baseurl");
+	}
 
 }
--- src/main/java/fr/openstreetmap/watch/EmailThread.java
+++ src/main/java/fr/openstreetmap/watch/EmailThread.java
@@ -1,9 +1,15 @@
 package fr.openstreetmap.watch;
 
 import java.util.List;
+import java.util.Properties;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.persistence.Query;
 
 import org.apache.log4j.Logger;
@@ -16,7 +22,7 @@ import fr.openstreetmap.watch.model.db.AlertMatch;
 @Service
 public class EmailThread {
 	@Autowired private DatabaseManager dbManager;
-	
+
 	volatile boolean stopping = false;
 	Thread t;
 
@@ -33,12 +39,12 @@ public class EmailThread {
 		t.notifyAll();
 		t.join();
 	}
-		
-	private void sendMail(Alert a) {
+
+	private void sendMail(Alert a) throws Exception {
 		String name = a.getUser().getScreenName();
 		String address = a.getUser().getEmailAddress();
 		logger.info("Sending mail to " + name + "<" + address + "> about " + a.getName());
-		
+
 		Query q = dbManager.getEM().createQuery(
 				"SELECT am from AlertMatch am where am.alert=?1 AND am.matchTimestamp > ?2");
 		q.setParameter(1, a);
@@ -46,19 +52,46 @@ public class EmailThread {
 
 		List<AlertMatch> matches = q.getResultList();
 		logger.info("Have "  +matches.size() + " matches");
+
+		Properties props = System.getProperties();
+		props.put("mail.smtp.host", "smtp.sfr.fr");
+		Session session = Session.getDefaultInstance(props, null);
+
+		Message msg = new MimeMessage(session);
+		msg.setSubject("[osm-watch] Notification for: " + a.getName());
+		msg.setFrom(new InternetAddress("OSM-Watch <no-reply@openstreetmap.fr>"));
+		msg.setRecipient(Message.RecipientType.TO, new InternetAddress(address, false));
+
+		StringBuilder content = new StringBuilder();
+
+		content.append("<p>Dear " + name + ",</p>");
+		content.append("<p>Here are the latest changesets matching your alert '" + name + "'</p>");
+
+		content.append("<ul>");
+		for (AlertMatch am : matches) {
+			content.append("<li>");
+			content.append("<a href=\"http://openstreetmap.org/browse/changeset/" + am.getChangesetId() + "\">Changeset " + am.getChangesetId() + "</a><br />");
+			content.append("</li>");
+		}
+		content.append("</ul>");
+
+		content.append("More details in the <a href=\"" + ApplicationConfigurator.getBaseURL() + "/api/rss_feed?key=" + a.getUniqueKey() + "\">RSS feed</a>");
+
+		msg.setContent(content.toString(), "text/html; charset=utf-8");
+		Transport.send(msg);
 	}
-	
+
 	class BGThread extends Thread {
 		public void run() {
 			logger.info("Starting email background thread");
 			while (!stopping) {
-				
+
 				dbManager.begin();
 				try {
 					Query q = dbManager.getEM().createQuery(
 							"SELECT a from Alert a WHERE a.emailEnabled=true AND a.lastEmailTimestamp <?1");
 					q.setParameter(1, System.currentTimeMillis() - 86400*1000);
-					
+
 					List<Alert> alerts = q.getResultList();
 
 					logger.info("Email background thread has " + alerts.size() + " mails to send");
@@ -68,13 +101,13 @@ public class EmailThread {
 						a.setLastEmailTimestamp(System.currentTimeMillis());
 						dbManager.getEM().persist(a);
 					}
-					
+
 					dbManager.commit();
 				}catch (Throwable t){
 					dbManager.rollback();
 					logger.error("Failed to run email run", t);
 				}
-				
+
 				try {
 					synchronized (this) {
 						wait(60000);
